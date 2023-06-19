4c5c01e070cbc01339f4f58f39be9eca8b72e62a
Ali-TBB <adebbach@gmail.com>
2023-06-19 14:50:55 +0100

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main-prit snigle digite number
+* followed by a new line
+* Return: always 0 (Success)
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}

d2efeb5d0099a6321c5345fb5b29204965089e99
Ali-TBB <adebbach@gmail.com>
2023-06-19 20:15:55 +0100

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main-print numbers
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num1 = 0 , num2;
+	while (num1 < 9)
+	{
+		for (num2 = num1 + 1;num2 <= 9 ;num2++)
+		{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			if (num1 != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		++num1;
+	}
+	putchar('\n');
+	return (0);
+}

478e22952a71e56b2b553a86f7dd11ae63c4f95d
Ali-TBB <adebbach@gmail.com>
2023-06-19 14:36:50 +0100

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,12 @@ int main(void)
 {
 	char ch;
 
-	for (ch 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch != 'q' && ch != 'e')
+		{
 			putchar(ch);
+		}
 	}
 	putchar('\n');
 	return (0);

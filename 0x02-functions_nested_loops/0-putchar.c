5b1eadc7ae79715b7efe9032808b353465fefb31
Ali-TBB <adebbach@gmail.com>
2023-06-20 10:13:53 +0100

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main-print putchar
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int n;
+
+	for (n = 0;n < 8 ;n++)
+		putchar(str[n]);
+	putchar('\n');
+	return (0);
+}

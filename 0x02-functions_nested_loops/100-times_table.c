36fb0ac797f99a8cd8c9c27e78b3f92aa04c100e
Ali-TBB <adebbach@gmail.com>
2023-06-21 18:15:57 +0100

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,7 +18,12 @@ void print_times_table(int n)
 			_putchar(' ');
 			if (n3 <= 9)
 				_putchar(' ');
-			else
+				_putchar(' ');
+			else if (n3 > 99 )
+				_putchar((n3 / 100) + '0');
+				_putchar((n3 / 10) + '0');
+			else if (n3 > 9 && n3 < 99)
+				_putchar(' ');
 				_putchar((n3 / 10) + '0');
 			_putchar((n3 % 10) + '0');
 		}

a8be6a9069ffa0f6b07f253b5f0d4a5bbd5f87ed
Ali-TBB <adebbach@gmail.com>
2023-06-21 15:23:21 +0100

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* jack_bauer-print every minute of day
+* Return: Aleways 0
+*/
+void jack_bauer(void);
+{
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}

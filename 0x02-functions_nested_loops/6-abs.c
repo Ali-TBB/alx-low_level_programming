970cd05eb437ed76295af65f0fe4513cfc86ebb9
Ali-TBB <adebbach@gmail.com>
2023-06-21 14:05:29 +0100

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _abs-print computes the absolute value of an integer
+* @n: integer number
+* Return: return n
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}

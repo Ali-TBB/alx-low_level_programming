dfc765212ab1839182b96c11be81b3444b83fd59
Ali-TBB <adebbach@gmail.com>
2023-06-21 18:47:58 +0100

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,31 +9,34 @@ void print_times_table(int n)
 {
 	int n1, n2, n3;
 
-	for (n1 = 0; n1 <= n; n1++)
+	if (n >= 0 && n <= 15)
 	{
-		_putchar('0');
-		for (n2 =  1; n2 <= n; n2++)
+		for (n1 = 0; n1 <= n; n1++)
 		{
-			n3 = n1 * n2;
-			_putchar(',');
-			_putchar(' ');
-			if (n3 <= 9)
+			_putchar('0');
+			for (n2 =  1; n2 <= n; n2++)
 			{
+				n3 = n1 * n2;
+				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
+				if (n3 <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (n3 > 99)
+				{
+					_putchar((n3 / 100) + '0');
+					_putchar(((n3 / 10)) % 10 + '0');
+				}
+				else if (n3 > 9 && n3 < 99)
+				{
+					_putchar(' ');
+					_putchar((n3 / 10) + '0');
+				}
+				_putchar((n3 % 10) + '0');
 			}
-			else if (n3 > 99)
-			{
-				_putchar((n3 / 100) + '0');
-				_putchar(((n3 / 10)) % 10 + '0');
-			}
-			else if (n3 > 9 && n3 < 99)
-			{
-				_putchar(' ');
-				_putchar((n3 / 10) + '0');
-			}
-			_putchar((n3 % 10) + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }

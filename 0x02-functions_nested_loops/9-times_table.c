95bd9b71240affae700982f9ce0c6e9127842b7e
Ali-TBB <adebbach@gmail.com>
2023-06-21 15:48:53 +0100

    task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* times_table-print the 9 times tsble
+* Return: Always 0
+*/
+void times_table(void)
+{
+	int n1, n2, n3;
+
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+		n3 = 0;
+		for (n2 = 0; n2 <= 9; n2++)
+		{
+			n3 += n1;
+			_putcahr(n3 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}

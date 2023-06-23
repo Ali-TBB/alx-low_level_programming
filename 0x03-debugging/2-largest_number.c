a05d91375a24d40eb885dc543a0bcb737b2a555a
Ali-TBB <adebbach@gmail.com>
2023-06-24 00:14:26 +0100

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,7 +13,7 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
-		largest = a;
+	largest = a;
 	}
 	else if (b > a && b > c)
 	{
@@ -21,7 +21,7 @@ int largest_number(int a, int b, int c)
 	}
 	else
 	{
-		largest = c;
+	largest = c;
 	}
 
 	return (largest);

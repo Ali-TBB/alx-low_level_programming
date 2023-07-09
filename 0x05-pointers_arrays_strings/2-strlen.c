76ae0e661a516575fe37ad68204971245886aebe
Ali-TBB <adebbach@gmail.com>
2023-07-09 11:58:12 +0100

    tassk 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* _strlen - retur length of a string
+* @s: cahr
+* Return: length
+*/
+int _strlen(char *s)
+{
+	int legth;
+	for(legth = 0; *s[legth] != '\0'; legth++);
+
+	return (legth);
+}

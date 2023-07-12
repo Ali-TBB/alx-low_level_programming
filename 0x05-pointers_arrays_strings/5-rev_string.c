f2ed9cfe7033e0ae5f30fb37416b046e17199ae3
Ali-TBB <adebbach@gmail.com>
2023-07-12 10:54:29 +0100

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* rev_string-function that revers string
+* Return: Always 0
+*/
+void rev_string(char *s)
+{
+	int a = 0, b, c;
+	char *c;
+
+	while (s[a] != '\0')
+	{
+		a++;
+
+	}
+	c = a
+	for (b = 0; a > 0;a--;b++)
+	{
+		s[a] = c[b];
+	}
+	for (b = 0; b < c;b++)
+	{
+		c[b] = s[b];
+	}
+}

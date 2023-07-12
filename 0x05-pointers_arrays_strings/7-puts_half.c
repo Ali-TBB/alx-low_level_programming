be4f6bbef132c1065dce39187ac0e029992961b7
Ali-TBB <adebbach@gmail.com>
2023-07-12 11:55:29 +0100

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* puts_half-function print half of string
+* @str: char
+* followed by a new line
+*/
+void puts_half(char *str)
+{
+	int a = 0, b;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	if (a % 2 == 0)
+	{
+		b = (a / 2);
+		for (; b <= a; b++)
+		{
+			putchar(str[b]);
+		}
+	}
+	else
+	{
+		b = ((a - 1) / 2);
+		for (; b <= a; b++)
+		{
+			putchar(str[b]);
+		}
+	}
+	putchar('\n');
+}

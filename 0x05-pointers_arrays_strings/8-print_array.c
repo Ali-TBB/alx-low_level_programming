9339242fe6b17acd933884918ddc88ebc41e58f5
Ali-TBB <adebbach@gmail.com>
2023-07-12 12:26:09 +0100

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* print_array-print all elements of array
+* @a: array
+* @n: number of element
+* followed by new line
+*/
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; n >= 0; n--, b++)
+	{
+		putchar(a[b]);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+}

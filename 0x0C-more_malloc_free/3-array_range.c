b96c6bfcdf737fcf1e301b8913a2e0ef91693f91
Ali-TBB <adebbach@gmail.com>
2023-07-13 19:01:58 +0100

    task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,16 +11,22 @@ int *array_range(int min, int max)
 	int *arr, b, a, c;
 
 	if (min > max)
+	{
 		return (NULL);
+	}
 	else
+	{
 		b = max - min;
 		arr = malloc(b + 1);
 		c = 0;
 		a = min;
 		while (c <= b)
+		{
 			arr[c] = a;
 			c++;
 			a++;
+		}
 		return (arr);
+	}
 }
 

0392712fc0512ed26ad7e31c76ab44d4483ab79b
Ali-TBB <adebbach@gmail.com>
2023-07-13 18:37:50 +0100

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	a = malloc(nmemd * size);
+	a = malloc(nmemb * size);
 	if (a == NULL)
 		return (NULL);
 	for (b = 0; b < (nmemb * size); b++)

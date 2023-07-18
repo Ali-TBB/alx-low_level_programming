def03e8a2b2e102124690eef277d997a02b542c4
Ali-TBB <adebbach@gmail.com>
2023-07-18 18:38:09 +0100

    task dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog- dog class
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif

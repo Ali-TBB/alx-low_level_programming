d4f360be33e154d095973f1bde72e66841d202c5
Ali-TBB <adebbach@gmail.com>
2023-07-19 16:08:05 +0100

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * array_iterator- fuctiion that print the integer of array
+ * @size: size of array
+ * @array: the array
+ * @action: fuction that will be print the integer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	for (int i = 0; i <= size; i++)
+		action(array[i]);
+}

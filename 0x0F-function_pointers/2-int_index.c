61d30aca1ce6a4d03e2b8c410623e9d2cabca34c
Ali-TBB <adebbach@gmail.com>
2023-07-19 17:56:27 +0100

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,7 +10,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (cmp != NULL || size > 0 || array != NULL)
+	if (cmp != NULL && size > 0 && array != NULL)
 	{
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]) != 0)

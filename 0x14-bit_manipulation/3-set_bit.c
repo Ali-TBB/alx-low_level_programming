02658d4709bfe53288303bff26c129a8c7858152
Ali-TBB <Ali-TBB@github.com>
2023-08-04 18:38:44 +0100

    task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_bit - returns the value of a bit at a given index.
+ * @n: checking bits
+ * @index: which to check bit
+ * Return: the value of the bit at index
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int d;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	d = 1 << index;
+	*n = n || d;
+    return (0);
+}

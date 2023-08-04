f62aedd035502f0b920f201d3e3d00dc2c5dde7c
Ali-TBB <Ali-TBB@github.com>
2023-08-04 18:42:32 +0100

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,6 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * set_bit - returns the value of a bit at a given index.
+ * set_bit - the value of a bit to 1 at a given index
  * @n: checking bits
  * @index: which to check bit
  * Return: the value of the bit at index
@@ -15,5 +14,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 	d = 1 << index;
 	*n = *n | d;
+
     return (0);
 }

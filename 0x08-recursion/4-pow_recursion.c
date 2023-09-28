ade7e9a3e347986db8f5b9e6a183a2e97fbe3364
Ali-TBB <Ali-Tbb@git.com>
2023-09-28 22:51:22 +0100

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  * @y: power of x
  * @x: int
  * Return: factorial of n

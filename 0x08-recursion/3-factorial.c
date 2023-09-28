77a3e01bb0ef1282b595bc60c31f9817a69791bb
Ali-TBB <Ali-Tbb@git.com>
2023-09-28 22:10:43 +0100

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,17 +7,15 @@
 
 int factorial(int n)
 {
-	if (n > 0)
+    int nm;
+	if (n < 0)
 	{
 		return (-1);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		return (1);
 	}
-	if (n > 0)
-	{
-		return (n * factorial(n - 1));
-	}
+	return (n * factorial(n - 1));
 
 }

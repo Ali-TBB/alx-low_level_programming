9fcf1e301645fb45e7a3bc6e2e91d0aeee2f312e
Ali-TBB <Ali-Tbb@git.com>
2023-10-29 20:23:05 +0100

    task 0

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - print number of arguments
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int n;
+
+	for (n = 0; n < argc; n++)
+	{
+		printf("%s\n", argv[n]);
+	}
+	return (0);
+}

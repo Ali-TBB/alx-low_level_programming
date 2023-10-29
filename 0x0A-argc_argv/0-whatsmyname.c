07cf49cd541b38dd2c820b3634a63dec49d0e2e2
Ali-TBB <Ali-Tbb@git.com>
2023-10-29 19:49:29 +0100

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/*
+* main- print name of the programme
+* Return: Always 0
+*/
+
+int main(int argc ,char* argv[])
+{
+    printf("%s", argv[0]);
+    return (0);
+}

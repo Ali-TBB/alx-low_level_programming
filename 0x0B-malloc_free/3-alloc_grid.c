18351941a42465effc8a21c11284f85f66798dba
Ali-TBB <Ali-Tbb@git.com>
2023-10-31 16:49:53 +0100

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,6 +16,8 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	grid = (int **)malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{

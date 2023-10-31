9863144ed60d1fcc17d9b0edac8c264f272db3c7
Ali-TBB <Ali-Tbb@git.com>
2023-10-31 06:17:10 +0100

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,10 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len1 = 0, len2 = 0, len, i;
 	char *strcont;
 
-	if (s1 == NULL && s2 == NULL)
-		return ("");
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[len1])
 	{
 		len1++;

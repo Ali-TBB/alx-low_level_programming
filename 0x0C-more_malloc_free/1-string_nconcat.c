1623cc0a901a392aabfd18ebbef4ddbdd8e6f9a4
Ali-TBB <Ali-Tbb@git.com>
2023-11-02 20:31:12 +0100

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,22 +13,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = 0, len, i, a;
 	char *str;
 
-	while (s2 && s1[len1])
+	while (s1 && s1[len1])
 	{
 		len1++;
 	}
-	while (s1 && s2[len2])
+	while (s2 && s2[len2])
 	{
 		len2++;
 	}
-	if (len2 < n)
+	if (n < len2)
 	{
-		str = malloc(sizeof(char) * (len2 + len1 + 1));
+		str = malloc(sizeof(char) * (len2 + n + 1));
 		len = len1 + len2 + 1;
 	}
 	else
 	{
-		str = malloc(sizeof(char) * (len1 + n + 1));
+		str = malloc(sizeof(char) * (len1 + len2 + 1));
 		len = len1 + n;
 	}
 	if (!str)

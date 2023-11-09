d8ad915a9fd7f0601e2614ab56b04dd03dfe9f94
Ali-TBB <Ali-Tbb@git.com>
2023-11-09 18:36:01 +0100

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,8 +2,5 @@
 #define _VARIABIC_FUNCTIONS_H_
 
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 
 #endif

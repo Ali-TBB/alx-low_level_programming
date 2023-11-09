94adb07c4a47730af8c996eb36f759b23c6f3eff
Ali-TBB <Ali-Tbb@git.com>
2023-11-09 20:42:49 +0100

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,16 +8,16 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	char *str;
 	unsigned int i;
-
 	va_list args;
 
 	va_start(args, n);
 
-	const char *str = va_arg(args, const char *);
 
 	for (i = 0; i < n; i++)
 	{
+		str = va_arg(args, char *);
 		if (str != NULL)
 		{
 			printf("%s", str);

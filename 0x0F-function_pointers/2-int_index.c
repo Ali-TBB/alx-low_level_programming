f1b5c0f33080f8912bef4fb65358cfad14c03eb2
Ali-TBB <Ali-Tbb@git.com>
2023-11-10 12:26:27 +0100

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,14 +11,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array == NULL || size <= 0)
-		return (-1);
-
-	while (i < size)
+	if (size > 0)
 	{
-		if (cmp(array[i]))
-			return (i);
-		i++;
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+				i++;
+			}
+		}
 	}
 	return (-1);
 }

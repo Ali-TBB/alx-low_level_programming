bc6b213120cc2f0a01cc20d5c5d3c81c8b5445e0
Ali-TBB <Ali-Tbb@git.com>
2023-11-16 23:51:05 +0100

    all task modify

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,9 +3,24 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node to the end of linked list
+ * _strlen - length of a string
+ * @str: string
+ *
+ * Return: length
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * add_node_end - adds a new node to the end
  * @head: double pointer to a linked list
- * @str: string to add to the new node
+ * @str: string to add
  *
  * Return: pointer to the new node
  */

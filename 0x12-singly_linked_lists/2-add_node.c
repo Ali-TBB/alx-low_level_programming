76008f80a8088c2444c76cc838ded75a1037cd64
Ali-TBB <Ali-Tbb@git.com>
2023-11-16 23:06:28 +0100

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - check the code
+ * @head: ponter to first lists
+ * @str: string
+ * Return: new node.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head = malloc(sizeof(list_t));
+
+	if (!new_head || !head)
+		return (NULL);
+	if (str)
+	{
+		new_head->str = strdup(str);
+		if (new_head->str)
+		{
+			free(new_head);
+			return (NULL);
+		}
+		new_head->len = _strlen(new_head->str);
+	}
+	new_head->next = *head;
+	*head = new_head;
+	return (new_head);
+}

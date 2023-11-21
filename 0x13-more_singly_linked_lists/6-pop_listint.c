cdbc06465d6c4f3e1858e40028f4bf600e62706c
Ali-TBB <Ali-Tbb@git.com>
2023-11-21 17:31:16 +0100

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete head of linkrd list
+ * @head: linked list.
+ * Return: size of list.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *new_head;
+	int n;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	new_head = *head;
+	*head = new_head->next;
+	n = new_head->n;
+	free(new_head);
+	return (n);
+}

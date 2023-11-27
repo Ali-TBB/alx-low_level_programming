5ef5f8ff82a76e251756e5c12675f650b786f9b7
Ali-TBB <Ali-Tbb@git.com>
2023-11-27 21:34:03 +0100

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len, w;
+	int fd, len = 0, w;
 
 	if (!filename)
 		return (-1);
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 		}
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 	if (w == -1 || fd == -1)
 		return (-1);

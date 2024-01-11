3a32bef5b1d8ab7083a4edb9a4d6922e59e96ade
Ali-TBB <Ali-Tbb@git.com>
2024-01-11 19:04:04 +0100

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t number = 0;
+	size_t number = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        number++;
-    }
-    return (number);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		number++;
+	}
+	return (number);
 }

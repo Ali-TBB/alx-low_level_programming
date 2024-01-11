1c2b95966985e22914d353c3ffc6f7d4448aecfe
Ali-TBB <Ali-Tbb@git.com>
2024-01-11 20:05:15 +0100

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_dlistint -  return the sum of data (n) of doubly linked list.
+ *
+ * @head: Pointer to the head of the doubly linked list.
+ * Return: the sum of data (n).
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    dlistint_t *node;
+    int sum = 0;
+
+    node = head;
+    while (node != NULL)
+    {
+    node = node->next;
+    sum += node->n;
+    }
+    return (sum);
+}

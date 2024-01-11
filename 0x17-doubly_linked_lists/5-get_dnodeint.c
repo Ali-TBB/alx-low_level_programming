c68b1a4cb842d53f385b5d78113c4d03d775dd9d
Ali-TBB <Ali-Tbb@git.com>
2024-01-11 20:00:37 +0100

    task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -5,6 +5,7 @@
  * get_dnodeint_at_index -  free doubly linked list.
  *
  * @head: Pointer to the head of the doubly linked list.
+ * @index: is the index of the node.
  * Return: void.
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
@@ -17,9 +18,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (head);
 
 	while (index > 0 && head != NULL)
-    {
-        head = head->next;
-        index--;
-    }
+	{
+		head = head->next;
+		index--;
+	}
 	return (head);
 }

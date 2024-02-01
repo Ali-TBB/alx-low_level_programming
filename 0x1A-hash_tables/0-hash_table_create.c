f9f78a9da76a2a12e298f49706409426abea0d5d
Ali-TBB <Ali-Tbb@git.com>
2024-02-01 17:38:11 +0100

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht[size];
-	int i;
+	hash_table_t *ht;
+	unsigned long int i;
 
-	for (i = 0; i < size; i++)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_node_t *));
+	if (ht->array == NULL)
 	{
-		ht[i] = malloc(sizeof(hash_table_t));
-		if (ht[i] == NULL)
-			return (NULL);
-		ht[i]->size = size;
+		free(ht);
+		return (NULL);
 	}
-	return (*ht);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+	return (ht);
 }

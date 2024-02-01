8100e424a064270001c8f4adb0e88327bc6bdbf3
Ali-TBB <Ali-Tbb@git.com>
2024-02-01 17:10:57 +0100

    hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_get - Retrieves the value associated with a key in a hash table.
  *
@@ -8,5 +9,18 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	
+	unsigned long int index;
+	hash_node_t *current;
+
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+        if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current->next = current;
+	}
+	return (NULL);
 }

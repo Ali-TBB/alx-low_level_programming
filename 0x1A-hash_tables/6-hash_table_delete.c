7a857bd206e0cb3a9b55ed5711eaf5dd9264c7e9
Ali-TBB <Ali-Tbb@git.com>
2024-02-01 18:07:46 +0100

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 
 /**
  * hash_table_delete - Deletes a hash table and its contents.
@@ -8,4 +9,23 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *current, *node;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			node = current;
+			current = current->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+		}
+		ht->array[i] = NULL;
+	}
+
+	free(ht->array);
+	free(ht);
 }
